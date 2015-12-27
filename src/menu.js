d162169ca52102f0153a5c5dfb22d9be22863689
pudilz <zack.pudil@dowjones.com>
2015-12-26 18:54:19 -0600

    Updated menu to tell player that he/she is waiting for player

--- src/menu.js
+++ src/menu.js
@@ -6,8 +6,10 @@ export default class Menu {
 	constructor(renderer) {
 		this.renderer = renderer;
 		this.ipAddress = "";
+		this.isWaiting = false;
 
 		userInput.addListener({ name: '1' }, () =>  {
+			this.isWaiting = true;
 			peers.create(() => {
 				this.gameStartCb(false)
 			});
@@ -21,15 +23,20 @@ export default class Menu {
 	}
 
 	render() {
+		let font = 'italic 20pt Calibri';
 		this.renderer.fillColor('#cc00ff');
+
 		this.renderer.text(20, 250, 'Ping', 'italic 140pt Calibri');
 		this.renderer.text(30, 400, 'Pong', 'italic 140pt Calibri');
 
 		//Draw options
-		var font = 'italic 20pt Calibri';
 		this.renderer.text(20, 430, "Press 1 to start a game.", font);
 		this.renderer.text(20, 460, "Press 2 to join a game.", font);
 		this.renderer.text(20, 490, 'Press 3 to exit game.', font)
+
+		if(this.isWaiting) {
+			this.renderer.text(20, 520, 'Waiting for players to join.', font);
+		}
 	}
 
 	onGameStart(options) {
