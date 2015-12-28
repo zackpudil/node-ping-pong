bb5c1cc1161738d9520a07ebfc27d5f292783bbc
paulmfischer <paul.fischer@ilmservice.com>
2015-12-28 12:20:53 -0600

    adding missed score file

---
+++ src/actors/score.js
@@ -0,0 +1,29 @@
+import GameConstants from '../game-constants';
+import gameState from '../gameState';
+
+export default class Score {
+	constructor(renderer, scale = 10) {
+    this.renderer = renderer;
+    this.pos = GameConstants.ScorePosition;
+    this.scale = scale;
+	}
+
+	render() {
+    this.renderScore();
+	}
+
+	update() {
+    this.renderScore();
+	}
+
+  renderScore() {
+    this.renderer.fillColor('#000000');
+		var font = '20pt Calibri';
+    this.renderer.text(this.pos.x, this.pos.y, this.getScore(), font);
+  }
+
+  getScore() {
+    let score = gameState.score;
+    return score.one + ' : ' + score.two;
+  }
+}
