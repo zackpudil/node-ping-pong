a2750e74fb3d7114e0e22600780f6dd7fa583dc1
pudilz <zack.pudil@dowjones.com>
2015-12-28 13:01:41 -0600

    Updated pause menu to send command to peer

--- src/pauseMenu.js
+++ src/pauseMenu.js
@@ -1,6 +1,7 @@
 import userInput from './user-input';
 import GameConstants from './game-constants';
 import gameState from './gameState';
+import peer from './peer';
 
 export default class PauseMenu {
 
@@ -11,15 +12,21 @@ export default class PauseMenu {
     this.gameStateChangeCb = options.startGameCb;
     this.endGameCb = options.exitGameCb;
 
-		userInput.addListener({ name: 'p' }, () =>  this.pauseGame());
+		userInput.addListener({ name: 'p' }, () =>  {
+			this.pauseGame()
+			peer.sendCommand('pause');
+		});
+
+		peer.onCommand('pause', () => this.pauseGame());
 	}
 
 	render() {
-    this.renderer.fillColor('#0497D7');
-    this.renderer.box(150, 75, 450, 150);
+		this.renderer.fillColor('#0497D7');
+		this.renderer.box(150, 75, 450, 150);
+
 
 		//Draw options
-    this.renderer.fillColor('#D70404');
+		this.renderer.fillColor('#D70404');
 		var font = 'italic 20pt Calibri';
 		this.renderer.text(175, 100, "Press \'p\' to unpause.", font);
 		this.renderer.text(175, 140, 'Press \'e\' to exit game.', font)
