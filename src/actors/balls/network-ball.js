304b673a170aecb6ae4fdd605a7bf08d3c2865c1
pudilz <zack.pudil@dowjones.com>
2016-01-01 23:56:10 -0600

    Updated network ball to change position on update.

--- src/actors/balls/network-ball.js
+++ src/actors/balls/network-ball.js
@@ -4,17 +4,20 @@ import Ball from './ball';
 export default class NetworkBall extends Ball {
 
 	constructor(x, y, renderer, scale = 10) {
-
 		super(x, y, renderer, scale);
+
+		this.networkPosition = null;
 
 		peer.onCommand('scored', (playerWhoScored) => {
 			super.scored(playerWhoScored);
 		});
 
 		peer.onCommand('ballUpdate', (pos) => {
-			this.pos = pos;
+			this.networkPosition = pos;
 		});
 	}
 
-	update() { }
+	update() { 
+		this.pos = this.networkPosition || this.pos;
+	}
 }
